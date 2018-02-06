6acf4a82a112b7de1338c029942dcde9e5f265c7
“Ashwin <ashwinvidiyala@gmail.com>
2018-02-06 12:10:45 -0600

    Finish MathDojo assignment

---
+++ Ruby Assignments/math_dojo.rb
@@ -0,0 +1,38 @@
+class MathDojo
+  def initialize
+    @result = 0
+  end
+
+  def add *number
+    for num in number
+      if num.class == Array
+        for n in num
+          @result += n
+        end
+      else
+        @result += num
+      end
+    end
+    self
+  end
+
+  def subtract *number
+    for num in number
+      if num.class == Array
+        for n in num
+          @result -= n
+        end
+      else
+        @result -= num
+      end
+    end
+    self
+  end
+
+  def result
+    puts @result
+  end
+end
+
+MathDojo.new.add(2).add(2, 5).subtract(3, 2).result
+MathDojo.new.add(1).add([3, 5, 7, 8], [2, 4.3, 1.25]).subtract([2,3], [1.1, 2.3]).result
