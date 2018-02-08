6bfaf6f0c13b17dc4f43607c86ec030ed3a5bee5
“Ashwin <ashwinvidiyala@gmail.com>
2018-02-08 08:08:51 -0600

    Finish Dojo Ninjas assignment

--- Rails/dojo_ninjas/app/models/dojo.rb
+++ Rails/dojo_ninjas/app/models/dojo.rb
@@ -2,4 +2,13 @@ class Dojo < ActiveRecord::Base
   has_many :ninjas
   validates :name, :city, :state, presence: true
   validates :state, length: { is: 2}
+
+  # Tried this but it doesn't work.
+  
+  before_destroy :destroy_ninjas
+
+  def destroy_ninjas
+    self.ninjas.destroy
+  end
+
 end
