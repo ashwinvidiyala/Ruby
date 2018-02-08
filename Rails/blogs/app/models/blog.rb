170547504ee9420ae9fe4d9695d44185583ad8df
“Ashwin <ashwinvidiyala@gmail.com>
2018-02-08 15:55:14 -0600

    Blogs assignment

--- Rails/blogs/app/models/blog.rb
+++ Rails/blogs/app/models/blog.rb
@@ -1,4 +1,7 @@
 class Blog < ActiveRecord::Base
+  has_many :owners
   has_many :posts
+  has_many :user_owners, through: :owners, source: :user
+  has_many :authors, through: :posts, source: :user
   validates :name, :description, presence: true
 end
---
+++ Rails/blogs/app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ActiveRecord::Base
+  has_many :messages
+  has_many :posts
+  has_many :owners
+  has_many :blogs_posted_on, through: :posts, source: :blog
+  has_many :blogs_owned, through: :owners, source: :blog
+end
