a45fc3c7e19b586cd9e4e819c6370927441f4d36
“Ashwin <ashwinvidiyala@gmail.com>
2018-02-13 16:27:37 -0600

    Finish Dojo Dashboard II assignment

--- Rails/dojo_dashboard/app/controllers/dojos_controller.rb
+++ Rails/dojo_dashboard/app/controllers/dojos_controller.rb
@@ -2,4 +2,24 @@ class DojosController < ApplicationController
   def index
     @dojos = Dojo.all
   end
+
+  def new
+  end
+
+  def create
+    dojo = Dojo.create( dojo_params )
+    unless dojo.valid?
+      flash[:errors] = dojo.errors.full_messages
+      return redirect_to new_dojo_path
+    end
+    flash[:success] = 'New Dojo successfully created'
+
+    return redirect_to root_path
+  end
+
+  private
+
+  def dojo_params
+    params.require(:dojo).permit(:branch, :street, :city, :state)
+  end
 end
