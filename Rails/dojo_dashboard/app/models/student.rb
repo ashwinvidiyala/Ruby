cc9836af674e9e61e2c72b148925d1b9927529b0
“Ashwin <ashwinvidiyala@gmail.com>
2018-02-16 10:56:28 -0600

    Add cohort model method to application

--- Rails/dojo_dashboard/app/models/student.rb
+++ Rails/dojo_dashboard/app/models/student.rb
@@ -3,4 +3,12 @@ class Student < ActiveRecord::Base
 
   validates :first_name, :last_name, :email, presence: true, length: { minimum: 2 }
   validates :dojo, presence: true
+
+  def cohort
+    start_of_day = self.created_at.beginning_of_day
+    end_of_day   = self.created_at.end_of_day
+    Student.all.where.not(id: self.id)
+               .where(created_at: start_of_day..end_of_day)
+               .where(dojo_id: self.dojo_id)
+  end
 end
